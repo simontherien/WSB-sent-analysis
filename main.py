0753ec7eeb75c876f560c0a6f1bfaa3cfdac71c0
simontherien <simon.therien@gmail.com>
2021-06-04 09:41:21 -0400

    Updated comments

--- main.py
+++ main.py
@@ -36,70 +36,70 @@ from keys import reddit_client_ID, reddit_secret_token, reddit_user_name, reddit
 import re
 
 
-def commentSentiment(ticker, urlT):
-    subComments = []
-    bodyComment = []
-    try:
-        check = reddit.submission(url=urlT)
-        subComments = check.comments
-    except:
-        return 0
-
-    for comment in subComments:
-        try:
-            bodyComment.append(comment.body)
-        except:
-            return 0
-
-    sia = SIA()
-    results = []
-    for line in bodyComment:
-        scores = sia.polarity_scores(line)
-        scores['headline'] = line
-
-        results.append(scores)
-
-    df = pd.DataFrame.from_records(results)
-    df.head()
-    df['label'] = 0
-
-    try:
-        df.loc[df['compound'] > 0.1, 'label'] = 1
-        df.loc[df['compound'] < -0.1, 'label'] = -1
-    except:
-        return 0
-
-    averageScore = 0
-    position = 0
-    while position < len(df.label) - 1:
-        averageScore = averageScore + df.label[position]
-        position += 1
-    averageScore = averageScore / len(df.label)
-
-    return (averageScore)
-
-
-def latestComment(ticker, urlT):
-    subComments = []
-    updateDates = []
-    try:
-        check = reddit.submission(url=urlT)
-        subComments = check.comments
-    except:
-        return 0
-
-    for comment in subComments:
-        try:
-            updateDates.append(comment.created_utc)
-        except:
-            return 0
-
-    updateDates.sort()
-    return (updateDates[-1])
-
-
-def get_date(date):
-    return dt.datetime.fromtimestamp(date)
+# def commentSentiment(ticker, urlT):
+#     subComments = []
+#     bodyComment = []
+#     try:
+#         check = reddit.submission(url=urlT)
+#         subComments = check.comments
+#     except:
+#         return 0
+#
+#     for comment in subComments:
+#         try:
+#             bodyComment.append(comment.body)
+#         except:
+#             return 0
+#
+#     sia = SIA()
+#     results = []
+#     for line in bodyComment:
+#         scores = sia.polarity_scores(line)
+#         scores['headline'] = line
+#
+#         results.append(scores)
+#
+#     df = pd.DataFrame.from_records(results)
+#     df.head()
+#     df['label'] = 0
+#
+#     try:
+#         df.loc[df['compound'] > 0.1, 'label'] = 1
+#         df.loc[df['compound'] < -0.1, 'label'] = -1
+#     except:
+#         return 0
+#
+#     averageScore = 0
+#     position = 0
+#     while position < len(df.label) - 1:
+#         averageScore = averageScore + df.label[position]
+#         position += 1
+#     averageScore = averageScore / len(df.label)
+#
+#     return (averageScore)
+#
+#
+# def latestComment(ticker, urlT):
+#     subComments = []
+#     updateDates = []
+#     try:
+#         check = reddit.submission(url=urlT)
+#         subComments = check.comments
+#     except:
+#         return 0
+#
+#     for comment in subComments:
+#         try:
+#             updateDates.append(comment.created_utc)
+#         except:
+#             return 0
+#
+#     updateDates.sort()
+#     return (updateDates[-1])
+#
+#
+# def get_date(date):
+#     return dt.datetime.fromtimestamp(date)
 
 
 # Is ticker a valid US company on 2021-06-02 ?
